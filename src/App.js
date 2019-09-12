acae5b28fbc9c820d47b2f0ffa449de8e28665ed
Márcio França Lima <marciofrancalima@hotmail.com>
2019-09-11 21:28:29 -0300

    chore: add json-server and materialize-css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
 
 function App() {
+  useEffect(() => {
+    // Init materialize js
+    M.AutoInit();
+  }, []);
   return <h1>Hello World</h1>;
 }
 
